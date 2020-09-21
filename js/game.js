1ef094dff11dda0b38ba1f30cd9bfa957061db59
ministrov <61511030+ministrov@users.noreply.github.com>
2020-09-21 20:58:45 +0300

    Added canvas

--- js/game.js
+++ js/game.js
@@ -1,25 +1,23 @@
 'use strict';
 
 const fireballSize = 22;
+const wizardWidth = 70;
+const wizardSpeed = 3;
 
 const getFireballSpeed = function (isMovingLeft) {
-  return isMovingLeft ? 2:5;
-}
-
-const wizardWidth = 70;
+  return isMovingLeft ? 2 : 5;
+};
 
 const getWizardHeight = function () {
   return 1.337 * wizardWidth;
-}
-
-const wizardSpeed = 3;
+};
 
 const getWizardX = function (gameFieldWidth) {
   return (gameFieldWidth - wizardWidth) / 2;
-}
-const getWizardY = function (gameFieldWidth) {
+};
+const getWizardY = function (gameFieldHeight) {
   return gameFieldHeight / 3;
-}
+};
 
 window.GameConstants = {
   Fireball: {
--- js/stat.js
+++ js/stat.js
@@ -1,12 +1,30 @@
+"use strict";
+
 const CLOUD_WIDTH = 500;
 const CLOUD_HEIGHT = 200;
 
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, 100, 50, '#fff');
+
+  ctx.fillStyle = 'red';
+  ctx.fillText('Вы', 110, 75);
+  ctx.fillRect(160, 60, 430, 20);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Иван', 110, 105);
+  ctx.fillRect(160, 90, 430, 20);
+
+  ctx.fillStyle = '#f2f2f2';
+  ctx.fillText('Юлия', 110, 135);
+  ctx.fillRect(160, 120, 430, 20);
+
+  ctx.fillStyle = '#f2f2f2';
+  ctx.fillText('Юлия', 110, 135);
+  ctx.fillRect(160, 120, 430, 20);
+};
+
 const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x,y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
-
-window.renderStatistics = function (ctx) {
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(100, 50, 500, 200);
-}
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
