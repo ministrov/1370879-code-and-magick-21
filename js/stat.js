3df3f7215e9f477d5be62fe7d38b4573a54d27a3
ministrov <61511030+ministrov@users.noreply.github.com>
2020-09-22 21:38:30 +0300

    Working on task 2

--- js/stat.js
+++ js/stat.js
@@ -1,30 +1,41 @@
-"use strict";
+'use strict';
 
 const CLOUD_WIDTH = 500;
 const CLOUD_HEIGHT = 200;
+const CLOUD_X = 100;
+const CLOUD_Y = 50;
+const GAP = 10;
+const FONT_GAP = 15;
+const TEXT_WIDTH = 50;
+const BAR_HEIGTH = 150;
+const BAR_WIDTH = 40;
 
-window.renderStatistics = function (ctx) {
-  renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, 100, 50, '#fff');
-
-  ctx.fillStyle = 'red';
-  ctx.fillText('Вы', 110, 75);
-  ctx.fillRect(160, 60, 430, 20);
-
-  ctx.fillStyle = '#000';
-  ctx.fillText('Иван', 110, 105);
-  ctx.fillRect(160, 90, 430, 20);
-
-  ctx.fillStyle = '#f2f2f2';
-  ctx.fillText('Юлия', 110, 135);
-  ctx.fillRect(160, 120, 430, 20);
-
-  ctx.fillStyle = '#f2f2f2';
-  ctx.fillText('Юлия', 110, 135);
-  ctx.fillRect(160, 120, 430, 20);
-};
-
-const renderCloud = function (ctx, x, y, color) {
+const renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function(ctx) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Вы', 170, 240);
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  ctx.fillRect(160, 60, 40, 150);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Кекс', 225, 240);
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(220, 60, 40, 150);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Катя', 280, 240);
+  ctx.fillStyle = 'grey';
+  ctx.fillRect(280, 60, 40, 150);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Игорь', 335, 240);
+  ctx.fillStyle = 'tomato';
+  ctx.fillRect(340, 60, 40, 150);
 };
