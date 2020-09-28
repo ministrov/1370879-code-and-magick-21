70b828e7344980320cb2f1d2d43b1a584c0c7ee0
ministrov <61511030+ministrov@users.noreply.github.com>
2020-09-28 21:20:51 +0300

    Fixing comment from my tutor

--- js/setup.js
+++ js/setup.js
@@ -3,12 +3,12 @@
 const WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф'];
 const WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц'];
 
-let userDialog = document.querySelector('.setup');
+const userDialog = userDialog.querySelector('.setup');
 userDialog.classList.remove('hidden');
-let similarListElement = document.querySelector('.setup-similar-list');
-let similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+const similarListElement = document.querySelector('.setup-similar-list');
+const similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-let wizards = [
+const wizards = [
   {name: WIZARD_NAMES[0], surname: WIZARD_SURNAME[0], colorCoat: 'rgb(241, 43, 107)', eyesColor: 'red'},
   {name: WIZARD_NAMES[1], surname: WIZARD_SURNAME[1], colorCoat: 'rgb(146, 100, 161)', eyesColor: 'black'},
   {name: WIZARD_NAMES[2], surname: WIZARD_SURNAME[2], colorCoat: 'rgb(56, 159, 117)', eyesColor: 'blue'},
